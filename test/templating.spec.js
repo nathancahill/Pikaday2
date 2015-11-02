a546a6638884b3c86ba2b0da833d92a79eb2e1df
Nathan Cahill <nathan@nathancahill.com>
2015-11-02 09:18:51 -0700

    use eminent for dom testing

--- test/templating.spec.js
+++ test/templating.spec.js
@@ -1,16 +1,16 @@
 
 import assert from 'assert';
-import jsdom from 'jsdom';
+import * as eminent from 'eminent';
 
 import { renderDay, renderWeek, renderTitle, renderTable } from '../src/lib/templating';
 
 
 describe('renderDay', () => {
-    let opts = {day: 31, month: 9, year: 2015}
+    it('renders a day in a cell with a button', () => {
+        let opts = {day: 31, month: 9, year: 2015}
 
-    let html = renderDay(opts);
+        let html = '<table>' + renderDay(opts) + '</table>';
 
-    jsdom.env(html, (err, window) => {
-        assert(!err);
+        eminent.domAttrsIsLike(html, 'table>tbody>tr>td[data-day=31 class=""]>button.pika-button.pika-day[type=button data-pika-year=2015 data-pika-month=9 data-pika-day=31]')
     });
 });
